a3b86b1315b91eb9252a2f9a49bdbc23e2b135c8
Mark Peters <mapeters101@earthlink.net>
2025-04-05 17:59:35 -0400

    Work on lesson081 from 0:55 to 3:30.

--- lesson081.py
+++ lesson081.py
@@ -7,5 +7,89 @@ def my_func(a, b=1):
 help(my_func)
 print('.'*80)
 
+def my_func(a, b=1):
+    'returns a * b'
+    return a * b
+
+help(my_func)
+print()
+
+def my_func(a, b=1):
+    '''returns a * b
+
+    some additional docs here
+
+    Inputs:
+
+    Outputs:
+    '''
+    return a * b
+
+help(my_func)
+print()
+
+def my_func(a, b=1):
+    # some comment here
+    '''returns a * b
+
+    some additional docs here
+
+    Inputs:
+
+    Outputs:
+    '''
+    return a * b
+
+help(my_func)
+print()
+
+def my_func(a, b=1):
+    # some comment here
+    'some string here'
+    '''returns a * b
+
+    some additional docs here
+
+    Inputs:
+
+    Outputs:
+    '''
+    return a * b
+
+help(my_func)
+print()
+
+def my_func(a, b=1):
+    # some comment here
+    i = 10
+    '''returns a * b
+
+    some additional docs here
+
+    Inputs:
+
+    Outputs:
+    '''
+    return a * b
+
+help(my_func)
+print()
+
+def my_func(a, b=1):
+    # some comment here
+    '''returns a * b
+
+    some additional docs here
+
+    Inputs:
+
+    Outputs:
+    '''
+    return a * b
+
+help(my_func)
+print()
+print('-'*80)
+
 
 
