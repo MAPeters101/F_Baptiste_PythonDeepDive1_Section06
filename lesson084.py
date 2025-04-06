ea2a4ed6667857fbc6f76a0a531b53e0c00ce2d9
Mark Peters <mapeters101@earthlink.net>
2025-04-06 13:21:47 -0400

    Work on lesson084 to 5:20.

--- lesson084.py
+++ lesson084.py
@@ -1,1 +1,17 @@
-# No code for lesson084.
+help(sorted)
+
+l = [1,5,4,10,9,6]
+print(l)
+print(sorted(l))
+print()
+l = ['c', 'B', 'D', 'a']
+print(l)
+print(sorted(l))
+print()
+print(ord('a'))
+print(ord('A'))
+print()
+
+print(sorted(l, key=lambda s: s.upper()))
+
+
