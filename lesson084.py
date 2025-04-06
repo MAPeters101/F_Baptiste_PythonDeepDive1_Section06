af4db725e80b08d1c62924775c67f91851cf330e
Mark Peters <mapeters101@earthlink.net>
2025-04-06 13:26:07 -0400

    Work on lesson084 from 5:20 to 8:20.

--- lesson084.py
+++ lesson084.py
@@ -13,5 +13,13 @@ print(ord('A'))
 print()
 
 print(sorted(l, key=lambda s: s.upper()))
+print('-'*80)
+
+d = {'abc': 200, 'def': 300, 'ghi':100}
+for e in d:
+    print(e)
+print(sorted(d))
+print(sorted(d, key= lambda e: d[e]))
+print('-'*80)
 
 
