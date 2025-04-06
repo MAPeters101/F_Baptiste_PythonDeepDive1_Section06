6089f4c044aafd417d4c34af9ece3e5fd7f08711
Mark Peters <mapeters101@earthlink.net>
2025-04-05 23:45:58 -0400

    Work on lesson081 from 5:30 to 11:40.

--- lesson081.py
+++ lesson081.py
@@ -94,4 +94,26 @@ print('-'*80)
 print(my_func.__doc__)
 print('-'*80)
 
+def my_func(a: 'annotation for a',
+            b: 'annotation for b' = 1) -> 'something with a long annotation for the return value':
+    """Documentation for my_func"""
+    return a * b
+help(my_func)
+print(my_func.__doc__)
+print(my_func.__annotations__)
+print()
+
+x = 3
+y = 5
+def my_func(a: 'some character', b=max(x,y)) -> 'character a repeated ' + str(max(x, y)) + ' times':
+    print(b)
+    return a * max(x, y)
+print(my_func('a'))
+print(my_func.__annotations__)
+print()
+
+x=10
+print(my_func('a'))
+print(my_func.__annotations__)
+print('-'*80)
 
