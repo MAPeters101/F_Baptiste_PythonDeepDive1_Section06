ef984373a71ed222768dd9126b86cab9caa08fb9
Mark Peters <mapeters101@earthlink.net>
2025-04-07 14:33:27 -0400

    Work on lesson087 from 16:45 to 19:00.

--- lesson087.py
+++ lesson087.py
@@ -103,5 +103,30 @@ import math
 print(inspect.getmodule(math.sin))
 print('-'*80)
 
+# Dummy code
+i = 100
+
+# TODO: Fix this function
+# Currently does nothing, but should do ...
+def my_func(a: "mandatory positional",
+            b: "optional positional" =1,
+            c=2,
+            *args: "add extra positional here",
+            kw1,
+            kw2=100,
+            kw3=200,
+            **kwargs: "provide extra kw-only here") -> "does nothing":
+    """This function does nothing but does have various parameters
+    and annotations"""
+    i = 10
+    j = 20
+    a = i + j
+    return a
+
+print(inspect.getcomments(my_func))
+print(my_func.__doc__)
+print('-'*80)
+
+
 
 
