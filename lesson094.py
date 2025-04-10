def64be6e13ae6a853fe283ee725e02ae83dc974
Mark Peters <mapeters101@earthlink.net>
2025-04-10 16:45:11 -0400

    Work on lesson094 from 2:50 to 4:00.

--- lesson094.py
+++ lesson094.py
@@ -12,3 +12,14 @@ print('-'*80)
 
 f = lambda x, y: my_func(10, x, y)
 f(100, 200)
+print('-'*80)
+
+f = partial(my_func, 10)
+f(20,30)
+print()
+f = partial(my_func, 10, 20)
+f(30)
+#f(10, 20)
+print('-'*80)
+
+
