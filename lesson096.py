3a744ee54af5475474deb95d2f2ca4e547dcac19
Mark Peters <mapeters101@earthlink.net>
2025-04-13 13:46:25 -0400

    Work on lesson096 from 1:45 to 3:15.

--- lesson096.py
+++ lesson096.py
@@ -3,3 +3,13 @@ from pprint import pprint
 
 pprint(dir(operator))
 print('-'*80)
+
+print(1+2)
+print(operator.add(1,2))
+print(operator.truediv(3,2))
+print(operator.floordiv(13,2))
+print(13//2)
+print('-'*80)
+
+
+
