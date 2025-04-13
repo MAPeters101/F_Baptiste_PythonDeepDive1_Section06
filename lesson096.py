631f6820d60bcd0fd5c8cff4994cd715c6a4ac2b
Mark Peters <mapeters101@earthlink.net>
2025-04-13 14:41:15 -0400

    Work on lesson096 from 4:30 to 6:25.

--- lesson096.py
+++ lesson096.py
@@ -14,4 +14,16 @@ print('-'*80)
 from functools import reduce
 print(reduce(lambda x,y: x*y, [1,2,3,4]))
 print(reduce(operator.mul, [1,2,3,4]))
+print('-'*80)
+
+print(operator.lt(10,3))
+from operator import is_
+print(is_('abc', 'def'))
+print(is_('abc', 'abc'))
+print()
+print(operator.truth([]))
+print(operator.truth([1]))
+print('-'*80)
+
+
 
